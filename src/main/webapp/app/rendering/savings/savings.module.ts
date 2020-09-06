7782cd2423e80850e740763d865810614fb33cf0
Romain DIeny <romain.dieny+github@gmail.com>
2020-09-06 21:13:17 +0200

    Demo ngx-echarts

--- src/main/webapp/app/rendering/savings/savings.module.ts
+++ src/main/webapp/app/rendering/savings/savings.module.ts
@@ -5,9 +5,13 @@ import { savingsRoute } from './savings-route.module';
 
 import { AccountManagerSharedModule } from '../../shared/shared.module';
 import { SavingsLineChartComponent } from './savings-line-chart.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import * as echarts from 'echarts';
+// import 'echarts/lib/theme/dark.js';
 
 @NgModule({
-  imports: [AccountManagerSharedModule, RouterModule.forChild(savingsRoute)],
+  imports: [AccountManagerSharedModule, RouterModule.forChild(savingsRoute), NgxEchartsModule.forRoot({ echarts })],
   declarations: [SavingsLineChartComponent],
   entryComponents: [],
 })
